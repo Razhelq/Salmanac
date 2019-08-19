307cb6b870b9d35a26ca5267e03c27f8f720df9c
Razhel <mateuszszpakowski@wp.pl>
2019-08-19 16:03:45 +0200

    admin model created

--- ShpacooAlmanac/shpacoo_portal/admin.py
+++ ShpacooAlmanac/shpacoo_portal/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Artist, Album
+from .models import Artist, Album, ScrappedData
 
 
 @admin.register(Artist)
@@ -14,3 +14,8 @@ class ArtistAdmin(admin.ModelAdmin):
 @admin.register(Album)
 class AlbumModel(admin.ModelAdmin):
     list_display = ['title', 'release_date', 'artist']
+
+
+@admin.register(ScrappedData)
+class ScrappedDataModel(admin.ModelAdmin):
+    last_display = ['title', 'release_album', 'artist_name']
