b36e5253ff0dd345953210f2d923f75c31900d4c
Razhel <mateuszszpakowski@wp.pl>
2019-08-19 16:04:50 +0200

    working on view for all alfbums finder

--- ShpacooAlmanac/shpacoo_portal/views.py
+++ ShpacooAlmanac/shpacoo_portal/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 import re
 from datetime import datetime
-from time import sleep
 
 import bs4, requests
 from django.contrib.auth import login, authenticate
@@ -13,7 +12,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from shpacoo_portal.forms import UserCreateForm, LoginForm, AddArtistForm
-from shpacoo_portal.models import Artist, Album
+from shpacoo_portal.models import Artist, Album, ScrappedData
 
 
 class TestView(View):
@@ -93,12 +92,90 @@ class FindAlbumView(View):
 class FindAlbumsView(View):
 
     def get(self, request):
+        HipHopDxAllScraper.get(self, request)
+        GeniusAllScraper.get(self, request)
         artists = Artist.objects.filter(user__username=request.user)
         for artist in artists:
-            HipHopDxScraper.get(self, request, artist.id)
-            GeniusScraper.get(self, request, artist.id)
-            sleep(60)
+            try:
+                scrapped_data = ScrappedData.objects.get(artist_name=artist.name)
+                Album.objects.create(title=scrapped_data.release_date, release_date=scrapped_data.release_date,
+                                     artist=artist)
+            except ObjectDoesNotExist:
+                pass
         return redirect('display-albums')
+
+
+class HipHopDxAllScraper(View):
+
+    def get(self, request):
+        current_month = datetime.now().strftime('%m')[1:]
+        for month in range(int(current_month), 13):
+            scrapped_website = requests.get(f'https://hiphopdx.com/release-dates?month={month}')
+            soup = bs4.BeautifulSoup(scrapped_website.text, features='html.parser')
+            albums = soup.select('.album a')
+            for album in albums:
+                if album.em:
+                    # sought_artist = Artist.objects.get(id=id)
+                    artist_name = ''.join(album.em.text.lower().split())
+                    date = album.find_all_previous('p')[1].text
+                    if re.search(r'[A-Z]{1}[a-z]{2}\W[0-9]{1,2}', date):
+                        date = datetime.strptime(date + ' 2019', '%b %d %Y').strftime('%Y-%m-%d')
+                    else:
+                        all_previous_p_tags = album.find_all_previous('p')
+                        for p_tag in all_previous_p_tags:
+                            date = re.search(r'[A-Z]{1}[a-z]{2}\W[0-9]{1,2}', str(p_tag))
+                            if date:
+                                date = datetime.strptime(date[0] + ' 2019', '%b %d %Y').strftime('%Y-%m-%d')
+                                break
+                    title = album.text.replace(album.em.text, '')
+                    try:
+                        ScrappedData.objects.filter(title=title)
+                    except ObjectDoesNotExist:
+                        ScrappedData.objects.create(title=title, release_date=date, artist_name=artist_name)
+
+
+class GeniusAllScraper(View):
+
+    def get(self, request):
+        current_month = datetime.now().strftime('%m')[1:]
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                  'November', 'December']
+        # sought_artist = Artist.objects.get(id=id)
+        for month in range(int(current_month), 13):
+            try:
+                scrapped_website = requests.get(
+                    f"https://genius.com/Genius-{months[month]}-2019-album-release-calendar-annotated")
+            except IndexError:
+                break
+            soup = bs4.BeautifulSoup(scrapped_website.text, features='html.parser')
+            page = soup.select('p')
+            splited_months = page[0].text.split('\n\n')
+            month_data = []
+            month_data_dict = {}
+            for splited_month in splited_months:
+                month_data.append(splited_month.split('\n'))
+                for day in range(len(month_data)):
+                    month_data_dict[month_data[day][0]] = []
+                    for album in range(1, len(month_data[day])):
+                        month_data_dict[month_data[day][0]].append(month_data[day][album])
+            # print(month_data_dict)
+            for date, albums in month_data_dict.items():
+                for album in albums:
+                    artist_name = album.split(' - ')[0].lower()
+                    # print('name ' + sought_artist.name.lower() + ' ' + artist_name.lower())
+                    artist_name = artist_name.lower()
+                    if date != 'TBA' and date:
+                        release_date = datetime.strptime(date.split('/')[0] + ' ' + date.split('/')[1] + ' 2019', '%m %d %Y').strftime('%Y-%m-%d')
+                    else:
+                        release_date = 0
+                    title = ' '.join(album.split(' - ')[1:(-1 if '/' in album.split(' - ')[-1] else 0)])
+                    # print(title)
+                    # print(release_date)
+                    # print(sought_artist)
+                    try:
+                        ScrappedData.objects.filter(title=title)
+                    except ObjectDoesNotExist:
+                        ScrappedData.objects.create(title=title, release_date=release_date, artist_name=artist_name)
 
 
 class HipHopDxScraper(View):
@@ -125,7 +202,7 @@ class HipHopDxScraper(View):
                                     break
                         title = album.text.replace(album.em.text, '')
                         try:
-                            Album.objects.get(title=title)
+                            Album.objects.filter(title=title)
                         except ObjectDoesNotExist:
                             Album.objects.create(title=title, release_date=date, artist=sought_artist)
 
@@ -169,7 +246,7 @@ class GeniusScraper(View):
                         print(release_date)
                         print(sought_artist)
                         try:
-                            Album.objects.get(title=title)
+                            Album.objects.filter(title=title)
                         except ObjectDoesNotExist:
                             Album.objects.create(title=title, release_date=release_date, artist=sought_artist)
             
